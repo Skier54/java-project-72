0c22b1e938735598d127c0a50cd314985c3765c6
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-08-31 17:56:34 +0700

    new file:   Dockerfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
     id("org.sonarqube") version "6.3.1.5724"
     id("io.freefair.lombok") version "8.14.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     java
     checkstyle
     application
@@ -16,13 +20,17 @@ repositories {
 }
 
 application {
-    mainClass.set("hexlet.code.Main")
+    mainClass.set("hexlet.code.App")
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("io.javalin:javalin-rendering:6.7.0")
     testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,20 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
