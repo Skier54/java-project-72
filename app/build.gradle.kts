1ab22ff68b2b23629462027753028e8f39f30d66
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-08-31 14:50:48 +0700

    modified:   build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
-    id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.3.1.5724"
+    id("io.freefair.lombok") version "8.14.2"
+    java
     checkstyle
     application
+    jacoco
 }
 
 group = "hexlet.code"
@@ -16,10 +20,21 @@ application {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Skier54_java-project-72")
+        property("sonar.organization", "skier54")
+    }
 }
